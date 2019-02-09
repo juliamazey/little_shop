7ddc0958fb51f4a6634054fced2534230327ed5d
JennicaStiehl <jennica.stiehl@gmail.com>
2019-02-09 13:53:58 -0700

    Adds shoulda-matchers, and rspec in rails_helper
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>
    Co-authored-by: Julia Marco <35079289+juliamarco@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+SimpleCov.Start
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -58,4 +61,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |where|
+      where.test_framework :rspec
+      where.library :rails
+    end
+  end
+
 end
