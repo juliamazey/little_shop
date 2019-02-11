b3782ae266c339c46997cfae15e248b2b5442e00
Trevor Nodland <trevor.nodland@gmail.com>
2019-02-11 15:32:17 -0700

    added tests for user and merchant roles
    Co-authored-by: Julia Marco <35079289+juliamarco@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe User do
       expect(admin.role).to eq("admin")
       expect(admin.admin?).to be_truthy
     end
+
+    it "can be created as a default user" do
+      user = User.create(username: "Whatever", password: 'yes', role: 0, email: "whatever@gmail.com", address: "larimer", city: "denver", state: "co", zip_code: 80124, active: 1)
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
+
+    it "can be created as a merchant" do
+      merchant = User.create(username: "Whatever", password: 'yes', role: 1, email: "whatever@gmail.com", address: "larimer", city: "denver", state: "co", zip_code: 80124, active: 1)
+
+      expect(merchant.role).to eq("merchant")
+      expect(merchant.merchant?).to be_truthy
+    end
   end
 end
