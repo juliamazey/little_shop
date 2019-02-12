9d9f97a2862662cb7a1d1cc877d5e5eb65d14d9c
Trevor Nodland <trevor.nodland@gmail.com>
2019-02-12 16:27:40 -0700

    finishes user story 20
    Co-authored-by: Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>

---
+++ spec/features/admin/user_show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'As and admin' do
+  context "when I visit a user's profile page" do
+    it "should see all the user info that a user would see" do
+      user_1 = create(:user)
+      admin = create(:user, role: 2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_user_path(user_1)
+      
+      expect(page).to have_content("#{user_1.username}")
+      expect(page).to have_content("#{user_1.email}")
+      expect(page).to_not have_content("#{user_1.password}")
+      expect(page).to_not have_content("#{user_1.password_digest}")
+      expect(page).to have_link ("Edit This Profile")
+    end
+  end
+end
