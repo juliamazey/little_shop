00cded5a4458c4591aad20a703fb59abf11bfd52
JennicaStiehl <jennica.stiehl@gmail.com>
2019-02-12 15:59:15 -0700

    adds user story #17
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

---
+++ spec/features/user/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "As a registered user" do
+  context "When I visit my own profile page" do
+    it "should see all of my profile data, except my password" do
+      user_1 = create(:user)
+
+      visit user_path(user_1)
+
+      expect(page).to have_content("#{user_1.username}")
+      expect(page).to have_content("#{user_1.email}")
+      expect(page).to_not have_content("#{user_1.password}")
+      expect(page).to_not have_content("#{user_1.password_digest}")
+      expect(page).to have_link ("Edit My Profile")
+    end
+  end
+end
+
+
+# And I see a link to edit my profile data
