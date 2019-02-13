eaaed4db26a3ff88a955276c1ac08a04e52244d0
Julia Marco <marcootj@gmail.com>
2019-02-12 18:05:44 -0700

    Added functionality for user story 14, only missing image links

--- spec/features/items/items_index_spec.rb
+++ spec/features/items/items_index_spec.rb
@@ -11,16 +11,11 @@ RSpec.describe 'when I visit /spices'  do
       it 'displays all items in the system except disabled items' do
         visit items_path
 
-          within "#item-#{@item_1.id}" do
+        within "#item-#{@item_1.id}" do
 
-        click_on @item_1.name
-        expect(current_path).to eq(item_path(@item_1))
-
+        expect(page).to have_content(@item_1.name)
         expect(page).to have_content("Price: #{@item_1.price}")
-
-        click_on @item_1.image
-        expect(current_path).to eq(item_path(@item_1))
-
+        expect(page).to have_xpath('//img[@src="http://theepicentre.com/wp-content/uploads/2012/07/cinnamon.jpg"]')
         expect(page).to have_content("Stock: #{@item_1.stock}")
         expect(page).to have_content(@item_1.user.username)
 
@@ -31,5 +26,20 @@ RSpec.describe 'when I visit /spices'  do
         expect(page).to have_content(@item_2.name)
         expect(page).to_not have_content(@item_3.name)
       end
+
+      it 'has links for the item name and image' do
+
+        visit items_path
+
+        click_on @item_1.name
+        expect(current_path).to eq(item_path(@item_1))
+
+        # visit items_path
+
+        # find(@item_1.image).click
+        # click_on @item_1.image
+        # expect(current_path).to eq(item_path(@item_1))
+      end
+
   end
 end
