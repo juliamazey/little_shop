6fd13071dcda995915e7dc5e40d70a175f675fc4
earl-stephens <stephensearl@hotmail.com>
2019-02-13 17:24:49 -0700

    completed user story 9 and tests pass
    Co-authored-by: Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
    Co-authored-by: Julia Marco <35079289+juliamarco@users.noreply.github.com>
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,23 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     session[:user_id] = @user.id
-    if user_params[:password] == user_params[:password_confirmation]
-      if @user.save
-        redirect_to user_path(@user)
+        # binding.pry
+    if User.find_by(email: user_params[:email])
+      flash[:failure] = "That email is already in use"
+      redirect_to new_user_path
+
+    else
+      if user_params[:password] == user_params[:password_confirmation]
+        if @user.save
+          redirect_to user_path(@user)
+        else
+          flash[:failure] = "All fields are required"
+          redirect_to new_user_path
+        end
       else
-        flash[:failure] = "All fields are required"
+        flash[:failure] = "Password confirmation failed"
         redirect_to new_user_path
       end
-    else
-      flash[:failure] = "Password confirmation failed"
-      redirect_to new_user_path
     end
   end
 
--- spec/features/user/user_new_spec.rb
+++ spec/features/user/user_new_spec.rb
@@ -59,5 +59,23 @@ RSpec.describe "user registration form" do
       expect(current_path).to eq(new_user_path)
     end
 
+    it "shows a flash message if email is not unique" do
+      user = create(:user)
+      visit new_user_path
+
+      fill_in "Username", with: "Posh"
+      fill_in "Password", with: "test"
+      fill_in "Password confirmation", with: "test"
+      fill_in "Address", with: "test"
+      fill_in "City", with: "test"
+      fill_in "State", with: "test"
+      fill_in "Zip code", with: 12345
+      fill_in "Email", with: "test1@mail.com"
+
+      click_on "Create User"
+
+      expect(page).to have_content("That email is already in use")
+      expect(current_path).to eq(new_user_path)
+    end
 
 end
