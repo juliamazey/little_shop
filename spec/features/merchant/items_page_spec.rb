07dedddf461783be828c2898a4ca41e1d1d9506a
Trevor Nodland <trevor.nodland@gmail.com>
2019-02-18 12:44:43 -0700

    Began writing tests for user story 55

--- spec/features/merchant/items_page_spec.rb
+++ spec/features/merchant/items_page_spec.rb
@@ -4,6 +4,7 @@ include ActionView::Helpers::NumberHelper
 RSpec.describe 'As a merchant' do
   before :each do
     @merchant = create(:user, role: 1)
+    @buyer = create(:user)
     @item_1 = create(:item, active: true, user: @merchant)
     @item_2 = create(:item, active: true, user: @merchant)
     @item_3 = create(:item, active: true)
@@ -26,32 +27,16 @@ RSpec.describe 'As a merchant' do
       expect(page).to have_content("Enable")
     end
 
-    it 'can add a new item' do
+    it 'can click on a link and be taken to a new item form' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
       visit merchant_dashboard_items_path
 
       expect(page).to have_content("Add Item")
-      expect(page).to have_content(@item_1.id)
-      expect(page).to have_content(@item_1.name)
-      expect(page).to have_xpath('//img[@src="http://theepicentre.com/wp-content/uploads/2012/07/cinnamon.jpg"]')
-      expect(page).to have_content("Price: #{number_to_currency(@item_1.price)}")
-      expect(page).to have_content("Stock: #{@item_1.stock}")
-      expect(page).to have_content(@item_2.name)
-      expect(page).to_not have_content(@item_3.name)
 
-      within "#item-#{@item_2.id}" do
-        expect(page).to have_content("Edit Item")
-        expect(page).to have_content("Disable")
-        expect(page).to_not have_content("Delete")
-      end
-      within "#item-#{@item_4.id}" do
-        expect(page).to have_content("Enable")
-      end
+      click_on "Add Item"
 
-      within "#item-#{@item_1.id}" do
-        expect(page).to have_content("Delete")
-      end
+      expect(current_path).to eq(merchant_new_item_path)
     end
   end
 end
