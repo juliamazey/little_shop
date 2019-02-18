29f542fa9a85d87e1ef33191ad7135f24f8f8f2d
Julia Marco <marcootj@gmail.com>
2019-02-18 08:30:04 -0700

    added method to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,19 @@ class Item < ApplicationRecord
 
   def average_fulfillment
   end
+
+
+  def self.top_five
+     Item.all.select("items.*, sum(order_items.order_quantity) as items_qty")
+     .joins(:order_items)
+     .group(:id, :item_id)
+     .order('items_qty desc')
+     .limit(5)
+  end
+
+  def quantity_sold
+    order_items.sum do |order_item|
+      order_item.order_quantity
+    end
+  end
 end
