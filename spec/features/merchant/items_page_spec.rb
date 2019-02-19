b059ed830862b7bda761c5489ffc7559953f3f20
Julia Marco <marcootj@gmail.com>
2019-02-19 09:05:10 -0700

    Added test for merchant enabling item

--- spec/features/merchant/items_page_spec.rb
+++ spec/features/merchant/items_page_spec.rb
@@ -16,10 +16,23 @@ RSpec.describe 'As a merchant' do
   end
 
   describe 'When I visit my /dashboard/items page' do
+
+    it 'can enable an item' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
+
+      visit merchant_index_items_path
+      within "#item-#{@item_4.id}" do
+        click_on "Enable"
+      end
+
+      expect(page).to have_content("This item is now available for sale.")
+      expect(page).to have_content("Disable")
+    end
+
     it 'can disable an item' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
-      visit merchant_items_path
+      visit merchant_index_items_path
       within "#item-#{@item_2.id}" do
         click_on "Disable"
       end
@@ -31,19 +44,19 @@ RSpec.describe 'As a merchant' do
     it 'can click on a link and be taken to a new item form' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
-      visit merchant_items_path
+      visit merchant_index_items_path
 
       expect(page).to have_content("Add Item")
 
       click_on "Add Item"
 
-      expect(current_path).to eq(merchant_dashboard_item_new_path)
+      expect(current_path).to eq(merchant_dashboard_item_new_path)  
     end
 
     it "can fill out a form to add a new item" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
-      visit merchant_dashboard_items_path
+      visit merchant_index_items_path
 
       click_on "Add Item"
 
@@ -53,8 +66,8 @@ RSpec.describe 'As a merchant' do
       fill_in "Stock", with: 20
 
       click_on "Create Item"
-      save_and_open_page
-      expect(current_path).to eq(merchant_dashboard_items_path)
+
+      expect(current_path).to eq(merchant_index_items_path)
       expect(page).to have_content("Item saved!")
       expect(page).to have_content("Thyme")
     end
