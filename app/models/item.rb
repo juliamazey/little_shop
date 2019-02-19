3dfedbd4715bc1897903bfca6527a00ba3302ce7
Trevor Nodland <trevor.nodland@gmail.com>
2019-02-19 08:47:53 -0700

    finishes story 56

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
+  validates_presence_of :price, :name, :description, :stock
 
   def self.select_active
     where(active:true)
