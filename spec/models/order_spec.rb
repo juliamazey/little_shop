519242e575e111dbbc846dc7a151e53a9a732d62
JennicaStiehl <jennica.stiehl@gmail.com>
2019-02-21 08:09:10 -0700

    adds more coverage

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -66,16 +66,32 @@ end
       order_items_1 = create(:order_item, item: item_1, order: order_1)
       order_items_2 = create(:order_item, item: item_2, order: order_1)
 
-      expect(order_1.grand_total).to eq(32)
+      expect(order_1.grand_total).to eq(132.0)
     end
     it "retrieves appropriate amount" do
       order_items = @order_1.grand_total
-      expect(order_items).to eq(22.0)
+      expect(order_items).to eq(132.0)
     end
     it "retrieves appropriate amount" do
       order_items = @order_1.total_items
       expect(order_items).to eq(6)
     end
- 
+    it "retrieves appropriate fulfillment amount" do
+      order_items = @order_1.fulfilled_items?
+      expect(order_items).to eq(true)
+    end
+    it "retrieves cancelled status" do
+      order_items = @order_1.cancelled?
+      expect(order_items).to eq(false)
+      expect(@order_1.status).to eq("shipped")
+    end
+    it "retrieves merchant_orders(merchant)" do
+      order_items = Order.merchant_orders(@merchant_1).length
+      expect(order_items).to eq(0)
+    end
+    it "retrieves merchant_orders(merchant)" do
+      order_items = Order.merchant_orders(@merchant_1).length
+      expect(order_items).to eq(0)
+    end
   end
 # end
